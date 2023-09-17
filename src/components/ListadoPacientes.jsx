153fe79a5a0a136fe51aa8a4b1fa88a194d99bea
Jeremias Gauto <jeremiasg_kc@hotmail.com>
2023-09-16 22:05:54 -0300

    listado de paciente con texto condicional

--- src/components/ListadoPacientes.jsx
+++ src/components/ListadoPacientes.jsx
@@ -4,22 +4,39 @@ import Paciente from './Paciente'
 function ListadoPacientes({ pacientes }) {
   
 
-
   return (
 
     <div className="sm:w-2/2 lg:w-3/5 md:h-screen overflow-y-scroll">
-      <h2 className='font-black text-3xl text-center'>Listado pacientes</h2>
-      <p className="text-xl mt-5 mb-10 text-center">
-          Administra tus{' '}
-          <span className="text-indigo-600 font-bold ">Pacientes y citas</span>
-      </p>
 
-      {pacientes.map((paciente) => (
-        <Paciente
-          key = {paciente.id}
-          paciente={paciente} />
-      ))}
+      {pacientes && pacientes.length ? (
+        <>
+                <h2 className='font-black text-3xl text-center'>Listado pacientes</h2>
+              <p className="text-xl mt-5 mb-10 text-center">
+                  Administra tus{' '}
+                  <span className="text-indigo-600 font-bold ">Pacientes y citas</span>
+              </p>
+
+              {pacientes.map((paciente) => (
+                <Paciente
+                  key = {paciente.id}
+                  paciente={paciente} />
+              ))}
+      </>
+
+      ) : (
+          <>
+            <h2 className='font-black text-3xl text-center'>No hay pacientes</h2>
+              <p className="text-xl mt-5 mb-10 text-center">
+                  Comienza agregando pacientes{' '}
+                  <span className="text-indigo-600 font-bold ">y apareceran en este lugar</span>
+              </p>
+          
+          
+          </>
+      )}
+
       
+     
       
 
 
